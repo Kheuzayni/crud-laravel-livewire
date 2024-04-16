bc0c0023226f1310f446503c97805dfdab07d975
Kheuzayni <bayecheikhfall249@gmail.com>
2024-04-16 15:43:45 +0000

    CRUD: List

--- resources/views/livewire/cart-list.blade.php
+++ resources/views/livewire/cart-list.blade.php
@@ -3,7 +3,7 @@
         <div class="card-header">
             <div class="row">
                 <div class="col">
-                    <h2>CRUD Laravel + livewire</h2> 
+                    <h2>CRUD Laravel & livewire</h2> 
                 </div>
                 <div class="col">
                     <a href= "" class= "btn btn-success btn-sm float-end">Add new</a>
