1dcc820447971393595507a0421c55cf3c153ffd
Kheuzayni <bayecheikhfall249@gmail.com>
2024-04-17 11:47:43 +0000

    crud : list car

--- app/Livewire/CarList.php
+++ app/Livewire/CarList.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Car;
 use Livewire\Component;
 
 class CarList extends Component
 {
+    public $all_cars;
+    
+    public  function mount(){
+        $this -> all_cars = Car::all();
+    }
+
     public function render()
     {
-        return view('livewire.car-list');
+        return view('livewire.car-list', [
+            'cars' => $this -> all_cars
+        ]);
     }
 }
